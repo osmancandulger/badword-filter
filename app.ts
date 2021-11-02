196464b60d0ecb22cb2ffd6fc264e8b37034e7ea
Osman Dülger <osmancandulger@gmail.com>
2021-11-02 21:34:47 +0300

    Feat: wordReplaceWith given mask string

--- app.ts
+++ app.ts
@@ -30,8 +30,40 @@ function checkIsValid(data: string, options: ValidationOptions) {
   }
   return isValid;
 }
+/**
+ * @description Get tweet response object
+ * @param {data}: any : tweet object element
+ */
+function wordReplaceWith(
+  data: string,
+  replacer: string,
+  options: ValidationOptions
+) {
+  let string: any[] = data.split(" ");
+  let badWordList: string[] = [];
+  let isValid: boolean = true;
+  let replacedString: string = "";
+  try {
+    for (let i = 0; i < Object.keys(options).length; i++) {
+      const item: string = Object.keys(options)[i];
+      badWordList = [].concat(require(`./data/tr/${item.toLowerCase()}.json`));
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  for (let i = 0; i < badWordList.length; i++) {
+    for (let j = 0; j < string.length; j++) {
+      if (badWordList[i].toLowerCase() === string[j].toLowerCase()) {
+        isValid = false;
+        string[string.indexOf(string[j])] = "*".repeat(string[j].length);
+        break;
+      }
+    }
+  }
+  return string.join(" ");
+}
 console.log(
-  checkIsValid("merhaba Değerli", {
+  wordReplaceWith("merhaba Jakoben", "*", {
     curse: true,
     negative: true,
     politics: true,
