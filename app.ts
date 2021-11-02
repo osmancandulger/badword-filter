85c0d541654ae4743515cae7dc56a7418dcd8fe7
Osman Dülger <osmancandulger@gmail.com>
2021-11-02 20:03:49 +0300

    Feat: CheckIsValid function to boolean returning

--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
-const curse = require("./data/tr/curse.json");
-const negative = require("./data/tr/negative.json");
-const politics = require("./data/tr/politics.json");
-const religion = require("./data/tr/religion.json");
-
+interface ValidationOptions {
+  curse?: boolean;
+  negative?: boolean;
+  politics?: boolean;
+  religion?: boolean;
+}
 /**
  * @description Get tweet response object
  * @param {data}: any : tweet object element
  */
-function checkIsValid(data: string) {
-  const badWordList = curse.concat(negative, politics, religion);
+function checkIsValid(data: string, options: ValidationOptions) {
+  const curse = require("./data/tr/curse.json");
+  let badWordList: string[] = [];
+  try {
+    // const negative = require("./data/tr/negative.json");
+    // const politics = require("./data/tr/politics.json");
+    // const religion = require("./data/tr/religion.json");
+    for (let i = 0; i < Object.keys(options).length; i++) {
+      const item: string = Object.keys(options)[i];
+      badWordList = [].concat(require(`./data/tr/${item.toLowerCase()}.json`));
+    }
+  } catch (error) {
+    console.error(error);
+  }
   for (let i = 0; i < badWordList.length; i++) {
     if (data.toLowerCase().includes(badWordList[i].toLowerCase())) {
       return false;
@@ -16,3 +29,4 @@ function checkIsValid(data: string) {
   }
   return true;
 }
+checkIsValid("", { curse: true, negative: true, politics: true });
